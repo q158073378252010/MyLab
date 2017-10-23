a3a882c5bcb44a0d528814f084190d323cbdf574
neoFelhz <neofelhz@gmail.com>
2017-10-23 23:30:01 +0800

    Upadte SW & Add snackbar

--- mat2ifw/sw.js
+++ mat2ifw/sw.js
@@ -3,14 +3,14 @@
     self.importScripts("https://cdnjs.cat.net/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js");
     self.toolbox.options.debug = false;
     self.toolbox.options.networkTimeoutSeconds = 5;
-    self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+    self.toolbox.router.get("/(.*)", self.toolbox.fastest, {
         origin: /cdnjs\.cat\.net/,
         cache: {
             name: "staticAssetsCache",
             maxEntries: 100
         }
     });
-    self.toolbox.router.get("/mat2ifw/(.*)", self.toolbox.cacheFirst, {
+    self.toolbox.router.get("/(.*)", self.toolbox.fastest, {
         origin: /s\.nfz\.yecdn\.com/,
         cache: {
             name: "staticAssetsCache",
